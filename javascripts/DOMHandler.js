307ca3e14c142d7c4be8df0c1fd52d3629209c44
blairvansant <blairbot@gmail.com>
2016-09-08 11:52:22 -0500

    almost complete, but missing final price

--- javascripts/DOMHandler.js
+++ javascripts/DOMHandler.js
@@ -8,39 +8,81 @@ var selectedTopping;
 var meatChooser = document.getElementById("meat");
 var breadChooser = document.getElementById("bread");
 var cheeseChooser = document.getElementById("cheese");
-var condimentsChooser = document.getElementById("condiments");
+var condimentsChooser = document.getElementById("condiment");
 var veggiesChooser = document.getElementById("veggies");
+var displayOrder = document.getElementById("final");
+var meatToppingPrice = 0;
+var breadToppingPrice = 0;
+var veggieToppingPrice = 0;
+var condimentsToppingPrice = 0;
+var cheeseToppingPrice = 0;
 
+displayOrder.innerHTML += " Your order: ";
 
 /* 
-  A <select> element broadcasts a change event, so you listen for it
-  and get the value of the topping from your augmented IIFE
+ A <select> element broadcasts a change event, so you listen for it
+ and get the value of the topping from your augmented IIFE
 */
 meatChooser.addEventListener("change", function(event) {
-  // Get the value chosen from the DOM
-  selectedTopping = event.target.value;
+ // Get the value chosen from the DOM
+ selectedTopping = event.target.value;
 
-  // Determine the price of the topping chosen
-
-  // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addMeat(selectedTopping);
+ // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addTopping(sandwichMaker.getMeatToppingPrice());
+ displayOrder.innerHTML += selectedTopping + " " + sandwichMaker.getMeatToppingPrice() + "<br>";
 });
 
 breadChooser.addEventListener("change", function(event) {
+ // Get the value chosen from the DOM
   selectedTopping = event.target.value;
 
+  sandwichMaker.addBread(selectedTopping);
+  console.log(selectedTopping);
+
+
+ sandwichMaker.addBread(selectedTopping);
+ // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addTopping(sandwichMaker.getBreadToppingPrice());
+ displayOrder.innerHTML += selectedTopping + " " + sandwichMaker.getBreadToppingPrice() + "<br>";
 });
 
 cheeseChooser.addEventListener("change", function(event) {
-  selectedTopping = event.target.value;
+ // Get the value chosen from the DOM
+ selectedTopping = event.target.value;
 
-});
-
-condimentsChooser.addEventListener("change", function(event) {
-  selectedTopping = event.target.value;
-
+ sandwichMaker.addCheese(selectedTopping);
+ // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addTopping(sandwichMaker.getCheeseToppingPrice());
+ displayOrder.innerHTML += selectedTopping + " " + sandwichMaker.getCheeseToppingPrice() + "<br>";
 });
 
 veggiesChooser.addEventListener("change", function(event) {
-  selectedTopping = event.target.value;
+ // Get the value chosen from the DOM
+ selectedTopping = event.target.value;
+
+ sandwichMaker.addVeggies(selectedTopping);
+ // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addTopping(sandwichMaker.getVeggiesToppingPrice());
+ displayOrder.innerHTML += selectedTopping + " " + sandwichMaker.getVeggiesToppingPrice() + "<br>";
+});
+
+condimentsChooser.addEventListener("change", function(event) {
+ // Get the value chosen from the DOM
+ selectedTopping = event.target.value;
+
+ sandwichMaker.addCondiments(selectedTopping);
+ // Add the topping to the SandwichMaker to increase the total price
+ sandwichMaker.addTopping(sandwichMaker.getCondimentsToppingPrice());
+ displayOrder.innerHTML += selectedTopping + " " + sandwichMaker.getCondimentsToppingPrice() + "<br>";
+});
+
+
+
+var btn = document.getElementById("btnSpecial");
+btn.addEventListener('click', function(event) {
+ event.preventDefault();
+ finalSandwichPrice = sandwichMaker.getTotalPrice();
+ displayOrder.innerHTML += "final:" + finalSandwichPrice;
 
 });
--- javascripts/meat.js
+++ javascripts/meat.js
@@ -1,34 +1,23 @@
 // This SandwichMaker IIFE augments the original one
-var SandwichMaker = (function(maker) {
-var meat = [];
-  // Private variable to store the different meat prices
-  var meatPrices = {"ham": 0.90, 
-                    "turkey": 1.50,
-                    "roast beef": 2.00,
-                    "bacon": 2.50
-                };
+var sandwichMaker = (function(maker) {
 
-  // Augment the original object with another method
-  maker.addMeat = function(meat) {
-    meat.push(meat);
-  };
+ // Private variable to store the different bread prices
+ var meatPrices = {
+     ham: 1,
+     turkey: 2,
+     roast: 3,
+     bacon: 4
+ };
 
- maker. removeMeat =function(meat){
- 	var index = meat.indexOf(meat);
- 		if(index !== -1){
- 			meat.splice(index, 1);
- 		}
- } 
-
-//add meat price
-maker.addMeatPrices =function(){
-	var totalMeatPrice = 0
-	for (var i = 0; i < meats.length; i++) {
-		totalMeatPrice += meatPrices[meat[i]]
-	}
-	return totalMeatPrice;
-}
-
-// Return the new, augmented object with the new method on it
-  return maker;
-})(SandwichMaker);
+ // Augment the original object with another method
+ maker.addMeat = function(pick) {
+   if (pick in meatPrices) {
+     meatToppingPrice = meatPrices[pick];
+   }
+ };
+ maker.getMeatToppingPrice = function() {
+   return meatToppingPrice;
+ };
+  // Return the new, augmented object with the new method on it
+ return maker;
+ })(sandwichMaker || {});
